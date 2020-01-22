9dd888d89f376ac45d812b37e8e0aeb38b75e4ad
vishwak <49344980+vishwakneelamegam@users.noreply.github.com>
2020-01-22 15:17:44 +0530

    Update gateway.py

--- gateway.py
+++ gateway.py
@@ -1,24 +1,43 @@
 import requests
 import json
-import time
-from workmateFileService import *
+import os
+from flask import Flask
+from flask import Flask, request
+from flask import Response
+from flask_cors import CORS
+from recordService import *
 #-----GATEWAY-----
 class gatewayService:
     def process(self,data):
         try:
+            fileObject = fileService()
             headers = {'Content-Type': 'application/json',}
             listsOfKey = ["id","proof","data"]
-            dictionary = json.loads(data)
-            k = dictionary.keys()
+            k = data.keys()
             if len(set(k) & set(listsOfKey)) == len(listsOfKey):
-                response = requests.post('http://"your_ip"/get', headers=headers, data=data)
+                response = requests.post('http://your_ip/get', headers=headers, data=data)
+                fileObject.write_file("gatewayLogs","Data : " + str(data))
                 return response.text
             else:
+                fileObject.write_file("gatewayError","(1) Error : " + str(data) + " Exception : wrong keys")
                 return False
         except Exception as e:
             fileObject = fileService()
-            fileObject.write_file("gatewayError",str(e))
+            fileObject.write_file("gatewayError","(2) Error : " + str(data) + " Exception : "  + str(e))
             return "error"
-data = '{"id":"your_id","proof":"your_proof","data":"data"}'
-obj = gatewayService()
-print(obj.process(data))
+#---FLASK---
+app = Flask(__name__)
+CORS(app)
+@app.route('/<types>', methods=['GET','POST'])
+def plan_input(types):
+    jsonData = request.get_json()
+    if types == "gateway":
+        obj = gatewayService()
+        return obj.process(jsonData)
+    else:
+        fileObject = fileService()
+        fileObject.write_file("gatewayError","(3) Error : " + str(types) + " Data : "  + str(jsonData))
+        return "error"
+if __name__ == '__main__':
+    port = int(os.getenv('PORT',80))
+    app.run(host='0.0.0.0',port=port,threaded=True)
