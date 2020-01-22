d4c6d41b788157e41261fc8c0f49106fbf617e9e
vishwak <49344980+vishwakneelamegam@users.noreply.github.com>
2020-01-22 15:20:04 +0530

    Update gateway.py

--- gateway.py
+++ gateway.py
@@ -1,3 +1,4 @@
+
 #-----GATEWAY-----
 class gatewayService:
     def process(self,data):
@@ -7,7 +8,6 @@ class gatewayService:
             listsOfKey = ["id","proof","data"]
             k = data.keys()
             if len(set(k) & set(listsOfKey)) == len(listsOfKey):
-                response = requests.post('http://your_ip/get', headers=headers, data=data)
                 fileObject.write_file("gatewayLogs","Data : " + str(data))
                 return response.text
             else:
