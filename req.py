90ca777a59ca0563cec629177a2362dad35c014a
vishwak <49344980+vishwakneelamegam@users.noreply.github.com>
2020-01-22 15:25:08 +0530

    Update req.py

--- req.py
+++ req.py
@@ -4,10 +4,7 @@ s = time.time()
 headers = {
     'Content-Type': 'application/json',
 }
-#data ='{"name":"asdf"}'
-data = '{"id":"pandi","proof":"1256","data":"he has fever"}'
-#data = '{"id":"abc","phone":"67890","key":"3d1ef8aedb2bc4c0239e3652e64e287b"}'
-#data = "hello"
+data = '{"id":"your_id","proof":"your_proof","data":"data"}'
 response = requests.post('http://localhost/gateway', headers=headers, data=data)
 print (response.text)
 e = time.time() - s
